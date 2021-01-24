77edf6eb6f9f3e22eb0ac2dacf84d4c7b10c2324
alejandro <alejandro@alejandros-MacBook-Pro.local>
2021-01-24 17:27:29 -0600

    db now on aws

--- dbConfig.js
+++ dbConfig.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('meinWorterbuch', 'postgres', 'alex', {
-    host: 'localhost',
+const sequelize = new Sequelize('meinWorterbuch', 'postgres', 'agr3at.Pa55w0rd', {
+    host: 'meinworterbuch.clvjecef6z3f.us-east-2.rds.amazonaws.com',
     port: '5432',
     dialect: 'postgres',/* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
     define: {
